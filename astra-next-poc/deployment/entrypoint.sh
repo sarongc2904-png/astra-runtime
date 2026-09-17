76dc30071fba11cae008ef2df3e3b0f1066f87ec
sarongc2904-png <sarongc2904@gmail.com>
2026-09-17 00:02:06 -0500

    ASTRA-NEXT-09 wire grounded creative QA flag

--- astra-next-poc/deployment/entrypoint.sh
+++ astra-next-poc/deployment/entrypoint.sh
@@ -13,5 +13,14 @@ else
   echo "[ASTRA_NEXT_ENTRYPOINT] knowledge_routing_audit_skipped run=false"
 fi
 
+if [ "${RUN_GROUNDED_CREATIVE_QA:-false}" = "true" ]; then
+  echo "[ASTRA_NEXT_ENTRYPOINT] grounded_creative_qa_start gate=ASTRA-NEXT-09"
+  CREATIVE_RC=0
+  node /opt/astra-next-benchmark/run_grounded_creative_output_qa.js || CREATIVE_RC=$?
+  echo "[ASTRA_NEXT_ENTRYPOINT] grounded_creative_qa_complete rc=${CREATIVE_RC}"
+else
+  echo "[ASTRA_NEXT_ENTRYPOINT] grounded_creative_qa_skipped run=false"
+fi
+
 wait "$BOOT_PID"
 exit $?
